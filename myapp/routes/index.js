109b1b41552e27a3389cd3d1fa7430cdaa7fd9b1
alloyking1 <alloyking1@gmail.com>
2021-03-26 08:33:41 -0400

    fixed some bugs

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,31 +1,39 @@
 var express = require('express');
 var router = express.Router();
-const Axios = require('axios');
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+ 
+axiosCookieJarSupport(axios);
 
 router.get('/', function (req, res, next){
   res.send('Home page');
 }),
 
-router.post('/demo1', async function(req, res, next) { 
-    const status = 200;
-     if (status === 200) {
-      await Axios.post(
-        'https://hooks.slack.com/services/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',
-        {
-          text: `
-            Webhook success 👍
-          `,
-          mrkdwn: true,
-        }
-      );
-    }  res.status(status).json({
-      response_message: {
-        message: `Hi${
-          req.query.name ? ` ${req.query.name}` : ''
-        } 👋 ~ This is where I would have my response or error message`,
-        code: status,
-      },
-    });
+router.post('/demo1', function (req, res) { 
+  const cookieJar = new tough.CookieJar();
+  const status = 200;
+    if (status === 200) {
+    axios.post(
+      'https://hooks.slack.com/services/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',
+      {
+        text: `
+          Webhook success 👍
+          check your git repo for any new issue
+        `,
+        mrkdwn: true,
+        jar: cookieJar,
+        withCredentials: true,
+      }
+    );
+  }  res.status(status).json({
+    response_message: {
+      message: `Hi${
+        req.query.name ? ` ${req.query.name}` : ''
+      } 👋 ~ This is where I would have my response or error message`,
+      code: status,
+    },
+  });
   
 });
 
